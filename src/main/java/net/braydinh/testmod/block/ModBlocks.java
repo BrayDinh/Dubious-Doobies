9639b4f3ea21e44e3cf7ef7c225bf2997d41df9d
BrayDinh <braydendinh1234@gmail.com>
2026-07-31 13:07:11 -0500

    Added Ore version of custom block, will later implement loot table and item drops

--- src/main/java/net/braydinh/testmod/block/ModBlocks.java
+++ src/main/java/net/braydinh/testmod/block/ModBlocks.java
@@ -2,9 +2,11 @@ package net.braydinh.testmod.block;
 
 import net.braydinh.testmod.TutorialMod;
 import net.braydinh.testmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
@@ -23,6 +25,11 @@ public class ModBlocks {
    public static final DeferredBlock<Block> BISMUTH_BLOCK = registerBLock("bismuth_block",
            () -> new Block(BlockBehaviour.Properties.of()
                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+
+   public static final DeferredBlock<Block> BISMUTH_ORE = registerBLock("bismuth_ore",
+           () -> new DropExperienceBlock(UniformInt.of(2, 4),
+                   BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
 
     private static <T extends Block> DeferredBlock<T> registerBLock(String name, Supplier<T> block){
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
