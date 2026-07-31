a8239a8b6dbeed82706433db36284a27d6a6b0c7
BrayDinh <braydendinh1234@gmail.com>
2026-07-31 12:54:17 -0500

    Added a custom block related to the added custom items.

--- src/main/java/net/braydinh/testmod/TutorialMod.java
+++ src/main/java/net/braydinh/testmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.braydinh.testmod;
 
+import net.braydinh.testmod.block.ModBlocks;
 import net.braydinh.testmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -35,6 +36,7 @@ public class TutorialMod {
         NeoForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -53,6 +55,10 @@ public class TutorialMod {
             event.accept(ModItems.VIBRATOR);
             event.accept(ModItems.BISMUTH);
             event.accept(ModItems.RAW_BISMUTH);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.BISMUTH_BLOCK);
         }
     }
 
